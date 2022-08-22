b1476b3a4f6951e90c3d26c331bc0a6e0a6d8fc3
Celeste Duguay <cdugu093@uottawa.ca>
2022-08-22 19:46:34 -0400

    Initial Commit

---
+++ Shared/VolleyLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyLinkApp.swift
+//  Shared
+//
+//  Created by Lixuan Guo on 2022-08-22.
+//
+
+import SwiftUI
+
+@main
+struct VolleyLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
